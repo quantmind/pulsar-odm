153754ace0307d798d31834f27e078bcf858f552
Luca Sbardella <luca.sbardella@gmail.com>
2015-03-09 10:24:23 +0000

    simplified setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,14 @@ import os
 import sys
 import json
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-package_name = 'odm'
-package_fullname = 'pulsar-odm'
 os.environ['pulsar_odm_setup'] = 'running'
-root_dir = os.path.dirname(os.path.abspath(__file__))
-package_dir = os.path.join(root_dir, package_name)
-odm = __import__(package_name)
+odm = __import__('odm')
 
 
 def read(fname):
-    with open(os.path.join(root_dir, fname)) as f:
+    with open(fname) as f:
         return f.read()
 
 
@@ -28,49 +23,9 @@ def requirements():
     return result
 
 
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-def get_rel_dir(d, base, res=''):
-    if d == base:
-        return res
-    br, r = os.path.split(d)
-    if res:
-        r = os.path.join(r, res)
-    return get_rel_dir(br, base, r)
-
-packages, data_files = [], []
-pieces = fullsplit(root_dir)
-if pieces[-1] == '':
-    len_root_dir = len(pieces) - 1
-else:
-    len_root_dir = len(pieces)
-
-for dirpath, _, filenames in os.walk(package_dir):
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)[len_root_dir:]))
-    elif filenames and not dirpath.endswith('__pycache__'):
-        rel_dir = get_rel_dir(dirpath, package_dir)
-        data_files.extend((os.path.join(rel_dir, f) for f in filenames))
-
-
 if __name__ == '__main__':
-
-    setup(name=package_fullname,
+    setup(name='pulsar-odm',
+          zip_safe=False,
           version=odm.__version__,
           author=odm.__author__,
           author_email=odm.__contact__,
@@ -78,9 +33,7 @@ if __name__ == '__main__':
           license='BSD',
           description=odm.__doc__,
           long_description=read('README.rst'),
-          packages=packages,
-          package_dir={package_name: package_name},
-          package_data={package_name: data_files},
+          packages=find_packages(exclude=('tests', 'tests.*')),
           install_requires=requirements(),
           classifiers=['Development Status :: 2 - Pre-Alpha',
                        'Environment :: Web Environment',
