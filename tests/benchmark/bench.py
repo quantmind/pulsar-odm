92ca54dc8e20100667a13b2c10e8ec9260f35522
Luca Sbardella <luca.sbardella@gmail.com>
2015-05-27 09:14:42 +0100

    benchmark writes a summary csv file

--- tests/benchmark/bench.py
+++ tests/benchmark/bench.py
@@ -1,9 +1,20 @@
+import csv
 from asyncio import async, wait, TimeoutError
 from random import randint
+from urllib.parse import urlparse
+from functools import reduce
 
 import pulsar
 from pulsar.apps.http import HttpClient
 from pulsar.apps.greenio import GreenPool
+from pulsar.utils.slugify import slugify
+
+
+# POOL_SIZES = [8, 16, 32, 64, 128, 256, 512, 1024]
+POOL_SIZES = [8, 16, 32, 64, 128, 256]
+TIMEOUT = 60
+REQUESTS = 1000
+FIELDNAMES = ['concurrency', 'requests', 'errors', 'time']
 
 
 class PostgreSql(pulsar.Setting):
@@ -30,67 +41,102 @@ class FillDB(pulsar.Setting):
     desc = "Fill database with random data"
 
 
-# POOL_SIZES = [8, 16, 32, 64, 128, 256, 512, 1024]
-POOL_SIZES = [8, 16, 32, 64]
-TIMEOUT = 15
-REQUESTS = 1000
-
-
 def wormup(worker, pool_size):
     worker.http = HttpClient(pool_size=pool_size, timeout=TIMEOUT)
     worker.requests = REQUESTS
     worker.logger.info('WORM UP')
-    yield from request(worker)
+    yield from request(worker, False)
     yield from worker.send('monitor', 'run', ready)
 
 
 def bench(worker):
     worker.requests = 2*REQUESTS
     worker.logger.info('BENCHMARKING')
-    yield from request(worker)
+    results = yield from request(worker)
+    return results
 
 
-def request(worker):
+def request(worker, log=True):
     url = worker.cfg.test_url
     loop = worker._loop
-    start = loop.time()
     number = worker.requests
-    worker.logger.info('Sending %d requests with %d concurrency to "%s"',
-                       number, worker.http.pool_size, url)
+    if log:
+        worker.logger.info('Sending %d requests with %d concurrency to "%s"',
+                           number, worker.http.pool_size, url)
     requests = [worker.http.get(url) for _ in range(number)]
+    start = loop.time()
     done, pending = yield from wait(requests, loop=loop)
+    taken = loop.time() - start
     assert not pending
     errors = 0
     for result in done:
         try:
-            result.result()
+            response = result.result()
+            if response.status_code != 200:
+                errors += 1
         except Exception:
             errors += 1
 
-    taken = loop.time() - start
-    worker.logger.info('Processed %d requests with %d errors in %.3f',
-                       number, errors, taken)
-    return {'taken': taken,
-            'number': number,
-            'errors': errors}
+    if log:
+        return {'time': taken,
+                'requests': number,
+                'errors': errors}
+
+
+def add(name):
+    return lambda a, b: a[name] + b[name]
 
 
 def ready(monitor):
     monitor.ready += 1
     if monitor.ready == monitor.cfg.workers:
+        try:
+            yield from run_benchmark(monitor)
+        finally:
+            monitor._loop.stop()
+
+
+def run_benchmark(monitor):
+    url = urlparse(monitor.cfg.test_url)
+    name = slugify(url.path) or 'home'
+    name = '%s_%d.csv' % (name, monitor.cfg.workers)
+    monitor.logger.info('WRITING RESULTS ON "%s"', name)
+    with open(name, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+        writer.writeheader()
         for pool_size in POOL_SIZES:
             size = pool_size//monitor.cfg.workers
             if size*monitor.cfg.workers != pool_size:
-                monitor.logger.error('Adjust workes so that pool sizes can be '
-                                     'evenly shared across them')
+                monitor.logger.error('Adjust workes so that pool sizes '
+                                     'can be evenly shared across them')
                 monitor._loop.stop()
+
+            # WORMUP
             requests = [monitor.send(worker, 'run', wormup, size) for
                         worker in monitor.managed_actors]
-            results = yield from wait(requests)
+            yield from wait(requests)
+
+            # BENCHMARK
             requests = [monitor.send(worker, 'run', bench) for
                         worker in monitor.managed_actors]
-            results = yield from wait(requests)
-        monitor._loop.stop()
+            results, pending = yield from wait(requests)
+            assert not pending, 'Pending requets!'
+            results = [r.result() for r in results]
+
+            summary = {'concurrency': pool_size}
+            for name in results[0]:
+                summary[name] = reduce(add(name), results)
+            writer.writerow(summary)
+
+            persec = summary['requests']/summary['time']
+            monitor.logger.info('%d concurrency - %d requests - '
+                                '%d errors - %.3f seconds - '
+                                '%.2f requests/sec',
+                                pool_size,
+                                summary['requests'],
+                                summary['errors'],
+                                summary['time'],
+                                persec)
 
 
 class Bench(pulsar.Application):
