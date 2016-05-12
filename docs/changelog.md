1af7d59995d4f344fa07ed1fd7ccfc020a818b1c
Luca Sbardella <luca@quantmind.com>
2016-05-12 09:20:00 +0100

    Release 0.5.0

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,4 +1,5 @@
 # Changelog
 
+* [Version 0.5](./history/0.5.html)
 * [Version 0.4](./history/0.4.html)
 * [Versions pre 0.4](./history/pre0.4.html)
--- setup.py
+++ setup.py
@@ -1,10 +1,66 @@
+#!/usr/bin/env python
+import sys
+import os
+import json
+import subprocess
+
 from setuptools import setup, find_packages
 
-import odm_config as config
+
+def extend(params, package=None):
+    if package:
+        path = os.path.dirname(__file__)
+        data = sh('%s %s package_info %s %s'
+                  % (sys.executable, __file__, package, path))
+        params.update(json.loads(data))
+
+    return params
+
+
+def read(name):
+    filename = os.path.join(os.path.dirname(__file__), name)
+    with open(filename) as fp:
+        return fp.read()
+
+
+def requirements(name):
+    install_requires = []
+    dependency_links = []
+
+    for line in read(name).split('\n'):
+        if line.startswith('-e '):
+            link = line[3:].strip()
+            if link == '.':
+                continue
+            dependency_links.append(link)
+            line = link.split('=')[1]
+        line = line.strip()
+        if line:
+            install_requires.append(line)
+
+    return install_requires, dependency_links
+
+
+def sh(command):
+    return subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=True,
+                            universal_newlines=True).communicate()[0]
+
+
+def package_info():
+    package = sys.argv[2]
+    if len(sys.argv) > 3:
+        sys.path.append(sys.argv[3])
+    os.environ['package_info'] = package
+    pkg = __import__(package)
+    print(json.dumps(dict(version=pkg.__version__,
+                          description=pkg.__doc__)))
 
 
 def run():
-    requires, links = config.requirements('requirements.txt')
+    requires, links = requirements('requirements.txt')
 
     meta = dict(
         name='pulsar-odm',
@@ -13,7 +69,7 @@ def run():
         url="https://github.com/quantmind/pulsar-odm",
         zip_safe=False,
         license='BSD',
-        long_description=config.read('README.rst'),
+        long_description=read('README.rst'),
         packages=find_packages(include=('odm', 'odm.*')),
         setup_requires=['pulsar'],
         install_requires=requires,
@@ -25,11 +81,19 @@ def run():
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
             'Topic :: Utilities'])
 
-    setup(**config.setup(meta, 'odm'))
+    setup(**extend(meta, 'odm'))
 
 
 if __name__ == '__main__':
-    run()
+    command = sys.argv[1] if len(sys.argv) > 1 else None
+    if command == 'package_info':
+        package_info()
+    elif command == 'agile':
+        from agile.app import AgileManager
+        AgileManager(description='Release manager for pulsar-odm',
+                     argv=sys.argv[2:]).start()
+    else:
+        run()
