de342c0be41223e9655e17af837f543f7d4d9590
Luca Sbardella <luca@quantmind.com>
2017-11-24 10:30:55 +0000

    release

--- ci/release.sh
+++ ci/release.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-VERSION="$(python setup.py --version)"
-echo ${VERSION}
+codecov
 
-python setup.py pypi --final
-python setup.py sdist
-twine upload dist/* --username lsbardel --password ${PYPI_PASSWORD}
-
-git push
-git tag -am "Release $VERSION [ci skip]" ${VERSION}
-git push --tags
+if [$TRAVIS_BRANCH == 'release']
+then
+    VERSION="$(python setup.py --version)"
+    echo ${VERSION}
+    echo PyPI source release
+    #
+    python setup.py pypi --final
+    python setup.py sdist
+    twine upload dist/* --username lsbardel --password ${PYPI_PASSWORD}
+    #
+    echo Create a new github tag
+    git push
+    git tag -am "Release $VERSION [ci skip]" ${VERSION}
+    git push --tags
+fi
