9b9e14610e43ddba02fb13cce01f9b5eb6289cfa
Luca Sbardella <luca@quantmind.com>
2017-11-24 10:26:18 +0000

    travis

--- ci/release.sh
+++ ci/release.sh
@@ -3,6 +3,10 @@
 VERSION="$(python setup.py --version)"
 echo ${VERSION}
 
+python setup.py pypi --final
+python setup.py sdist
+twine upload dist/* --username lsbardel --password ${PYPI_PASSWORD}
+
 git push
 git tag -am "Release $VERSION [ci skip]" ${VERSION}
 git push --tags
